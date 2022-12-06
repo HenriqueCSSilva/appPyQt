f2c93ace8fce3ec440f70f12263701941ef54e03
giulia nunes <giulia.arocontabil@gmail.com>
2022-12-06 08:19:09 -0300

    update

--- principal.py
+++ principal.py
@@ -4,32 +4,46 @@ from PyQt5.QtWidgets import QMessageBox
 import sys
 import os
 import base
+import pandas as pd
 
-class Janela(QtWidgets.QMainWindow, base.Ui_MainWindow, ):
+
+class Janela(QtWidgets.QMainWindow, base.Ui_MainWindow):
     def __init__(self, parent=None):
+
         super(Janela, self).__init__(parent)
         self.setupUi(self)
-        self.Btn_botao1_2.clicked.connect(self.exibir) 
-        self.Btn_botao1_3.clicked.connect(self.abrirPasta)
+        self.Btn_botao_exibir.clicked.connect(self.exibir) 
+        self.Btn_botao_3pontos.clicked.connect(self.abrirPasta)
+        self.Btn_botao_mostrar.clicked.connect(self.mostrar)
         
         
-    def exibir (self): # função botão exibir
-        nome = self.txt_nome.text()
+    def exibir(self): # função botão exibir
+        
         patrimonio = self.txt_patrimonio.text()
-        email = self.txt_email.text()
-        estado = self.txt_estado.text()
-        
-        if(email == '' or email == None):
-            QMessageBox.about(self, "AVISO" , "Verifique o campo de e-mail" )
+        modelo = self.txt_modelo.text()
+        processador = self.txt_processador.text()
+        memoria = self.txt_memoria.text()
+        status = self.txt_status.text()
+        usuario = self.txt_usuario.text()
+        setor = self.txt_usuario.text()
+
+        campos = [patrimonio, modelo, processador, memoria, status, usuario, setor]
+
+        for item in campos:
+            if(item == '' or item == None):
+                QMessageBox.about(self, "AVISO" , "Campo vazio! \n" "por favor, preencha os campos")
+                return
         else:
-            print(nome,"\n",patrimonio,"\n",email,"\n",estado)
-        #def  clickMethod (self) :
-            #QMessageBox.about(self, "AVISO" , "O botão foi clicado" )
+            print (patrimonio,"\n",modelo,"\n",processador,"\n",memoria,"\n",status,"\n",status,"\n",usuario,"\n",setor)
     
     def abrirPasta(self): # função botão ...
-        path = os.getcwd() + '\base_de_dados'
+        path = os.getcwd() + '\\base_de_dados' 
         os.startfile(path)
-        
+
+    def mostrar(self): # função botão mostar
+        path = os.getcwd() + '\\base_de_dados' + '\\basePatrimonio.xlsx'
+        tabela = pd.read_excel(path)
+        print(tabela)
         
 def main():
     app = QApplication(sys.argv)
